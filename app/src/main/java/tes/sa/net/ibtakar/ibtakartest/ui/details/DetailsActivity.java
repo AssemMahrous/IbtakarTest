14689d26747cb1515c9f4c5a343721b5939c707a
assem911 <assem@fixawy.net>
2018-12-25 21:29:23 +0200

    adding details activity

---
+++ app/src/main/java/tes/sa/net/ibtakar/ibtakartest/ui/details/DetailsActivity.java
@@ -0,0 +1,31 @@
+package tes.sa.net.ibtakar.ibtakartest.ui.details;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import tes.sa.net.ibtakar.ibtakartest.R;
+
+public class DetailsActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(getIntent().getStringExtra("name"));
+        }
+
+    }
+
+}
